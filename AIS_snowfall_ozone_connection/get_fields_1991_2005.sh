750f8684fd9218ae3200b061cbca51202ad1eb65
Jeremy Fyke <fyke@lanl.gov>
2017-08-31 09:10:09 -0600

    In progress plotting work

--- AIS_snowfall_ozone_connection/get_fields_1991_2005.sh
+++ AIS_snowfall_ozone_connection/get_fields_1991_2005.sh
@@ -1,24 +1,20 @@
-#!/bin/csh -fx
+#!/bin/bash
 
+vars=(PRECC PRECL)
+vars2=(Z3 UQ VQ ICEFRAC TREFHT PSL TMQ PS)
 
-#loop over ensemble members
-set nr = 1
-while ( $nr < 100 )
-   if ( $nr < 10) then
-      set nrr = "00"$nr
-   else
-      set nrr = "0"$nr
-   endif
+for nrr in 001 010 012 015 018 020 022 028; do
 
+   echo '           #########Processing ensemble number: '$nrr '#########'
+
+#FIXED-O3 LE PROCESSING
+   
    rm -f tmp*.nc
 
-   set vars = ( PRECC PRECL )
-   set vars2 = ( ICEFRAC TREFHT PSL TMQ )
-
-   foreach vr ( $vars )
-      #cdo shifttime,-1mo ../../../data/fixed-ozone/$vr/b.e11.B20LE_fixedO3_$nrr.cam.h0.$vr.195501-200512.nc tmp$vr".nc"
+   for vr in ${vars[@]}; do
       cdo -a setcalendar,standard /glade/p/umit0006/FESD/fixedO3/atm/proc/tseries/monthly/$vr/b.e11.B20LE_fixedO3_$nrr.cam.h0.$vr.195501-200512.nc tmp$vr".nc"
-   end
+   done
+
    cdo merge tmpPRECC.nc tmpPRECL.nc tmp3.nc
 
    cdo expr,'PRECIP=PRECL+PRECC' tmp3.nc tmp4.nc
@@ -30,71 +26,57 @@ while ( $nr < 100 )
    cdo -b F64 selyear,1991/2005 tmp2.nc DJF_PRECIP_$nrr".nc"
    cdo -b F64 yearsum mon_PRECIP_$nrr".nc" year_PRECIP_$nrr".nc" # to mm/year
 
-   foreach vr ( $vars2 )
-   cdo -a setcalendar,standard /glade/p/umit0006/FESD/fixedO3/atm/proc/tseries/monthly/$vr/b.e11.B20LE_fixedO3_$nrr.cam.h0.$vr.195501-200512.nc tmp$vr".nc"
-   cdo -b F64 selyear,1991/2005 tmp$vr".nc" mon_$vr"_"$nrr".nc" 
-   cdo -b F64 yearmean -selseas,DJF mon_$vr"_"$nrr".nc" DJF_$vr"_"$nrr".nc"
-   cdo -b F64 yearmean mon_$vr"_"$nrr".nc" year_$vr"_"$nrr".nc"
-   end
+   mv *PRECIP_"$nrr".nc files_1991_2005/no-ozone
 
-   mv *$nrr*nc files_1991_2005/no-ozone/
+   for vr in ${vars2[@]}; do
+      cdo -a setcalendar,standard /glade/p/umit0006/FESD/fixedO3/atm/proc/tseries/monthly/$vr/b.e11.B20LE_fixedO3_$nrr.cam.h0.$vr.195501-200512.nc tmp$vr".nc"
+      cdo -b F64 selyear,1991/2005 tmp$vr".nc" mon_$vr"_"$nrr".nc" 
+      cdo -b F64 yearmean -selseas,DJF mon_$vr"_"$nrr".nc" DJF_$vr"_"$nrr".nc"
+      cdo -b F64 yearmean mon_$vr"_"$nrr".nc" year_$vr"_"$nrr".nc"
+      mv -f *$vr"_"$nrr".nc" files_1991_2005/no-ozone
+   done
+   
+   
+   
+   
+   
+#CESM LE PROCESSING
 
-   @ nr = $nr + 1
+   rm -f tmp*.nc
 
-end
+   for vr in ${vars[@]}; do      
+      if [ $nrr == 001 ]; then
+         cdo -a setcalendar,standard /glade/p/cesmLE/CESM-CAM5-BGC-LE/atm/proc/tseries/monthly/$vr/b.e11.B20TRC5CNBDRD.f09_g16.$nrr.cam.h0.$vr.185001-200512.nc tmp$vr".nc"
+      else 
+         cdo -a setcalendar,standard /glade/p/cesmLE/CESM-CAM5-BGC-LE/atm/proc/tseries/monthly/$vr/b.e11.B20TRC5CNBDRD.f09_g16.$nrr.cam.h0.$vr.192001-200512.nc tmp$vr".nc"
+      fi
+   done
 
-#loop over ensemble members
-set nr = 1
-while ( $nr < 200 )
-if ( $nr < 10) then
-set nrr = "00"$nr
-else
-if ( $nr < 100) then
-set nrr = "0"$nr
-else
-set nrr = ""$nr
-endif
-endif
+   cdo merge tmpPRECC.nc tmpPRECL.nc tmp3.nc
+
+   cdo expr,'PRECIP=PRECL+PRECC' tmp3.nc tmp4.nc
+
+   cdo -b F64 mulc,86400000. tmp4.nc tmp5.nc # m/s to mm/day
+   cdo -b F64 muldpm tmp5.nc tmp.nc # to mm/month
+   cdo -b F64 selyear,1991/2005 tmp.nc mon_PRECIP_$nrr".nc" 
+   cdo -b F64 yearsum -selseas,DJF tmp.nc tmp2.nc
+   cdo -b F64 selyear,1991/2005 tmp2.nc DJF_PRECIP_$nrr".nc"
+   cdo -b F64 yearsum mon_PRECIP_$nrr".nc" year_PRECIP_$nrr".nc" # to mm/year
+
+   mv *PRECIP_"$nrr".nc   files_1991_2005/lens
+
+   for vr in ${vars2[@]}; do
+      if [ $nrr == 001 ]; then
+         cdo -a setcalendar,standard /glade/p/cesmLE/CESM-CAM5-BGC-LE/atm/proc/tseries/monthly/$vr/b.e11.B20TRC5CNBDRD.f09_g16.$nrr.cam.h0.$vr.185001-200512.nc tmp$vr".nc"
+      else    
+         cdo -a setcalendar,standard /glade/p/cesmLE/CESM-CAM5-BGC-LE/atm/proc/tseries/monthly/$vr/b.e11.B20TRC5CNBDRD.f09_g16.$nrr.cam.h0.$vr.192001-200512.nc tmp$vr".nc"
+      fi      
+      cdo -b F64 selyear,1991/2005 tmp$vr".nc" mon_$vr"_"$nrr".nc" 
+      cdo -b F64 yearmean -selseas,DJF mon_$vr"_"$nrr".nc" DJF_$vr"_"$nrr".nc"
+      cdo -b F64 yearmean mon_$vr"_"$nrr".nc" year_$vr"_"$nrr".nc"
+      mv -f *$vr"_"$nrr".nc" files_1991_2005/lens
+   done
+   
+done
 
 rm -f tmp*.nc
-
-set vars = ( PRECC PRECL )
-set vars2 = ( ICEFRAC TREFHT PSL TMQ )
-
-foreach vr ( $vars )
-
-if ( $nr == 1 ) then
-	cdo -a setcalendar,standard /glade/p/cesmLE/CESM-CAM5-BGC-LE/atm/proc/tseries/monthly/$vr/b.e11.B20TRC5CNBDRD.f09_g16.$nrr.cam.h0.$vr.185001-200512.nc tmp$vr".nc"
-else
-	cdo -a setcalendar,standard /glade/p/cesmLE/CESM-CAM5-BGC-LE/atm/proc/tseries/monthly/$vr/b.e11.B20TRC5CNBDRD.f09_g16.$nrr.cam.h0.$vr.192001-200512.nc tmp$vr".nc"
-endif
-
-end
-
-cdo merge tmpPRECC.nc tmpPRECL.nc tmp3.nc
-
-cdo expr,'PRECIP=PRECL+PRECC' tmp3.nc tmp4.nc
-
-cdo -b F64 mulc,86400000. tmp4.nc tmp5.nc # m/s to mm/day
-cdo -b F64 muldpm tmp5.nc tmp.nc # to mm/month
-cdo -b F64 selyear,1991/2005 tmp.nc mon_PRECIP_$nrr".nc" 
-cdo -b F64 yearsum -selseas,DJF tmp.nc tmp2.nc
-cdo -b F64 selyear,1991/2005 tmp2.nc DJF_PRECIP_$nrr".nc"
-cdo -b F64 yearsum mon_PRECIP_$nrr".nc" year_PRECIP_$nrr".nc" # to mm/year
-
-foreach vr ( $vars2 )
-if ( $nr == 1 ) then
-	cdo -a setcalendar,standard /glade/p/cesmLE/CESM-CAM5-BGC-LE/atm/proc/tseries/monthly/$vr/b.e11.B20TRC5CNBDRD.f09_g16.$nrr.cam.h0.$vr.185001-200512.nc tmp$vr".nc"
-else
-	cdo -a setcalendar,standard /glade/p/cesmLE/CESM-CAM5-BGC-LE/atm/proc/tseries/monthly/$vr/b.e11.B20TRC5CNBDRD.f09_g16.$nrr.cam.h0.$vr.192001-200512.nc tmp$vr".nc"
-endif
-cdo -b F64 selyear,1991/2005 tmp$vr".nc" mon_$vr"_"$nrr".nc" 
-cdo -b F64 yearmean -selseas,DJF mon_$vr"_"$nrr".nc" DJF_$vr"_"$nrr".nc"
-cdo -b F64 yearmean mon_$vr"_"$nrr".nc" year_$vr"_"$nrr".nc"
-end
-mv *$nrr*nc files_1991_2005/lens/
-
-@ nr = $nr + 1
-
-
-end
